de64d0fc4a9db3867e14204b4512685b60ffba3a
Will <wricks2023.16@gmail.com>
2026-01-22 00:05:43 +0000

    Basic menu for install/setup script set up.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Initial installation manager for my Linux Mint install.
+
+## Overview
+This is a command line interface that will install the defaults for my system, and other optional applications and systems. 
+Installing something that is already on the system will result in the install not happening, unless the overwrite option is selected for that application (or all applications).
+
+## Interface
+I use whiptail to display a GUI for this program.
+
+## To-do
+Make all apps open in a seperate window - particularly browser websites.
--- setup.sh
+++ setup.sh
@@ -1,83 +1,22 @@
-# Run the script using the bash shell. 
 #!/bin/bash
 
-# STEPS
-# Ensure script stops on error.
-# Update package list and upgrade packages.
-# Install all apt packages using packages.txt.
-# Remove unecessary apt packages.
-# Run manual install script (for things that require latest/specific version) (neovim).
-# Set up config files - perhaps use stow.
-# Miscellaneus tasks
-    # Delete unecessary home drive folders.
-    # Set up firewall.
-    # Set up git email and username (for commit data).
-    # Set up ssh keys.
-    # Start ssh agent.
-    # Autoremove unecessary packages.
+default=$1
+option=null
 
-# Stop script execution if an error occurs. 
-set -e
+display_main_menu () {
+    eval `resize`
+    whiptail --title "Setup menu - configure what to install within each catagory." --menu "Options are selected by default, enter false as the parameter of the setup script to have all options selected to false." $LINES $COLUMNS $(($LINES - 8)) \
+    "asd" "" \
+    "Applications" "" \
+    "Games" "" \
+    "Setup/install selected" "" \
+    "Exit" "" 
+    # The option selected is printed to stderr.
+}
 
-# If updating the package database succeeds, update packages to their latest version.
-sudo apt update && sudo apt upgrade -y
-echo "[PROGRESS] Package database updated and all packages upgraded."
-
-# Read all packages to install with apt into packages file, then install.
-while IFS= read -r package || [[ -n "$package" ]]; do
-    # If package isn't empty and doesnt start with #
-    if [[ -z "$package" || "$package" =~ ^# ]]; then
-        continue
-    fi
-    echo "Installing $package..."
-    sudo apt install -y "$package"
-done < ./GeneralSetup/install_packages.txt
-
-while IFS= read -r package || [[ -n "$package" ]]; do
-    # If package isn't empty and doesnt start with #
-    if [[ -z "$package" || "$package" =~ ^# ]]; then
-        continue
-    fi
-    echo "Removing $package..."
-    sudo apt remove -y "$package"
-done < ./GeneralSetup/remove_packages.txt
-
-./GeneralSetup/manual_package_installs.sh
-
-./GeneralSetup/configs_setup.sh
-
-# Delete unecessary (empty) folders.
-rm -rf ~/Desktop/ ~/Games/ ~/Pictures/ ~/Videos/ ~/Templates/ ~/Music/ ~/Public/
-echo "[PROGRESS] Deleted unecessary home directory folders."
-
-# Set up basic firewall, allowing outging traffic but blocking incoming traffic.
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw status verbose
-echo "[PROGRESS] Set up basic firewall - outgoing allowed, incoming blocked."
-
-git config --global user.email "wricks2023.16@gmail.com"
-git config --global user.name "Will"
-
-# Set up simple ssh keys.
-if [[ ! -d ~/.ssh ]]; then 
-    ssh-keygen -t ed25519
-fi
-
-if [ ! -d ~/Remote ]; then
-	mkdir -p ~/Remote
-fi
-echo "[PROGRESS] SSH keys generated (if not already)."
-
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
-echo "[PROGRESS] Started ssh agent and added private key (need user to add public key to github)."
-
-sudo apt autoremove
-echo "[PROGRESS] Removed unecessary dependencies."
-
-echo "[PROGRESS] To apply some changes, please log out and log into the i3 desktop environment, would you like to do this automatically? (Y/n)"
-read input
-if [[ "$input" == "Y" || "$input" == "y" || -z "$input" ]]; then
-	loginctl terminate-session "$XDG_SESSION_ID"
-fi
+# Starting program.
+while [ "$option" != "Exit" ] 
+do
+    option="$(display_main_menu 3>&1 1>&2 2>&3)"
+    echo $option
+done
