419dc9f01251abe0f1e026f202755bdd14272dc6
Will <wricks2023.16@gmail.com>
2026-01-23 21:28:01 +0000

    Can now read from json, and load the game menu with names and desciptions

--- setup.sh
+++ setup.sh
@@ -5,43 +5,68 @@ option=null
 
 display_main_menu() {
     eval `resize`
-    whiptail --title "Setup menu - configure what to install within each catagory." --menu "Options are selected by default, enter false as the parameter of the setup script to have all options selected to false." $LINES $COLUMNS $(($LINES - 8)) \
-    "asd" "" \
+    whiptail --title "Setup menu - configure what to install within each catagory." --ok-button "DO NOT PRESS" --cancel-button "Exit" --menu "Options are selected by default, enter false as the parameter of the setup script to have all options selected to false." $LINES $COLUMNS $(($LINES - 8)) \
+    "Select All" "" \
+    "Deselect All" "" \
     "Applications" "" \
     "Games" "" \
-    "Setup/install selected" "" \
+    "Setup/Install Selected" "" \
     "Exit" "" 
-    # The option selected is printed to stderr.
+    # The option selected is printed to stderr. Which has been redirected to stdcout.
 }
 
 display_game_menu() {
-whiptail --title "Check list example" --checklist \
-    "Choose user's permissions" 20 78 4 \
-    "NET_OUTBOUND" "Allow connections to other hosts" ON \
-    "NET_INBOUND" "Allow connections from other hosts" OFF \
-    "LOCAL_MOUNT" "Allow mounting of local devices" OFF \
-    "REMOTE_MOUNT" "Allow mounting of remote devices" OFF
+    options=()
+    for ((i=0; i<${#gamesName[@]}; i++)); do
+        description="$(jq --arg name "${gamesName[i]}" '.[] | select(.name == $name) | .description' ./data/games.json)"
+        options+=( \
+            "\"${gamesName[i]}\"" \
+            "$description" \
+            "${gamesSelected[i]}" \
+        )
+    done
+    
+    echo ${options[@]}
+    eval `resize`
+    whiptail --title "Games install options" --checklist "Choose what game software to install" $LINES $COLUMNS $(($LINES - 8)) \
+    "${options[@]}"
 }
 
 # Starting program.
+sudo apt install jq -y # For interpreting json files.
+
+# Read name data from json files, this will be used later to get specific data for each menu. Uses parallel arrays to store whether the corresponding option is enabled. 1st array names, 2nd array whether its enabled, 3rd array scripts, these can be changed when overwrite is enabled/disabled.
+mapfile -t gamesName < <(jq -r '.[].name' ./data/games.json)
+gamesScript=$(jq '.[].installScript' ./data/games.json) # All entries must have a name and script, everything else is optional.
+gamesSelected=()
+for ((i=0; i<${#gamesName[@]}; i++)); do
+    gamesSelected[i]="OFF"
+done
+#echo ${gamesName[*]}
+
 while [ "$option" != "Exit" ] 
 do
-    option="$(display_main_menu 3>&1 1>&2 2>&3)"
-    echo $option
+    option="$(display_main_menu 3>&1 1>&2 2>&3)" # stderr is redirected to stdout before display_main_menu is called.
 
-    case $option in 
+    #echo $option
+    case $option in
+        "Select All")
+            ;;
+        "Deselect All")
+            ;;
         "Applications")
             ;;
-        "Games")
+        "Games")                
             display_game_menu
             ;;
         "Setup/install selected")
             ;;
-        "Exit")
+        "Exit")                
             exit
             ;;
         *)
+            echo "Invalid option or exit selected"
             exit
-            ;;
+            ;;        
     esac
 done
