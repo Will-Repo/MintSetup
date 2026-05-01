fdc8b88a2f307e128b011a9ca350b6d3ce1ecdda
Will <wricks2023.16@gmail.com>
2026-05-01 02:18:49 +0100

    fzf menu progress

--- README.md
+++ README.md
@@ -2,19 +2,25 @@
 
 ## Overview
 This is a command line interface that will install the defaults for my system, and other optional applications and systems. 
-This uses [gum][https://github.com/charmbracelet/gum] to display the interface.
+This uses fzf and the terminal to display the interface.
 Installing something that is already on the system will result in the install not happening.
 
+## Controls
+### Selecting checkboxes
+Selecting options using enter automatically updates the checkbox, but causes screen flicker (fzf must reload).
+Selecting checkboxes using tab marks boxes as selected, but the checkboxes dont update until the next enter input.
+
 ## Dependencies
-[gum][https://github.com/charmbracelet/gum] must be installed.
+[fzf][https://github.com/junegunn/fzf] must be installed.
 [jq][https://github.com/jqlang/jq] must be installed.
-Bash shell program must be at /bin/bash - or modify first line of the script to point to your bash program.
+tac must be installed.
+Bash shell program (version 4 or later) must be at /bin/bash - or modify first line of the script to point to your bash program.
 
 ## Recommended Install Locations
 Install general applications to ~/.local/share/
 
 ## To-do
-Make all apps open in a seperate window - particularly browser websites.
+Finalise the menu formmat.
+Display menu using fzf.
 Add exit prompt that asks whether they want to install selected.
-Hitting escape unselects every option - fix this unintended behavior.
-Split into setup script, and additional downloads.
+Decide whether to replace tac with array reversal.
--- setup.sh
+++ setup.sh
@@ -3,23 +3,51 @@
 checkDependencies() {
     exit="false"
 
-    if ! command -v gum >/dev/null; then
-        printf "%s\n" "Gum is not installed, see README for more info."
+    if ! command -v fzf >/dev/null; then
+        printf "%s\n" "fzf is not installed, see README for more info."
         exit="true"
     fi
 
     if ! command -v jq >/dev/null; then
-        printf "%s\n" "JQ is not installed, see README for more info."
+        printf "%s\n" "jq is not installed, see README for more info."
         exit="true"
     fi
 
-    if "$exit" = "true"; then
+    if ! command -v tac >/dev/null; then
+        printf "%s\n" "tac is not installed, see README for more info."
+        exit="true"
+    fi
+
+    if [[ "$exit" == "true" ]]; then
         exit
     fi
 
     echo "All dependencies met"
 }
 
+showCategories() {
+    dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    mapfile -t categories < <(jq -r '.[].category' $dir/data/categories.json)
+
+    #TODO: Use jq to get categories and description.
+    while true; do
+        # Show menu list of categories, and their contents and description in preview.
+        choice=$(printf '%s\n' "${categories[@]}" | tac | fzf --preview "jq -r --arg category {} '.[] | select(.category == \$category) | .description' \"$dir/data/categories.json\"")
+
+        case $choice in 
+            "Install Selected")
+                printf "Installing Selected"
+                ;;
+            "Exit")
+                printf "Exiting"
+                exit
+                ;;
+        esac
+    done
+}
+
+#printf '%s\n' "${categories[@]}" | fzf --multi --bind 'tab:toggle' --preview 'echo {}'
 
 # Start of function calls and program flow.
 checkDependencies
+showCategories
