8ce66c2f6e12e0a15d636bbbaf5eb04e913a4c51
Will <wricks2023.16@gmail.com>
2026-05-02 21:46:10 +0100

    Install Selected now shows currently selected

--- setup.sh
+++ setup.sh
@@ -109,7 +109,18 @@ showCategories() {
     #TODO: Use jq to get categories and description.
     while true; do
         # Show menu list of categories, and their contents (including currently checkboxed) and description in preview. TODO: Show full list of selected options under install selected - get rid of checkboxes, just names.
-        choice=$(printf "%s\n" "${categories[@]}" "${defaults[@]}" | tac | fzf --header="Current data directory: $dataPath" --preview "jq -r --arg category {} '.[] | select(.category == \$category) | .description' \"$dataPath/categories.json\" \"$dir/.default.json\"; cat "$dir/.temp/{}" 2>/dev/null")
+        choice=$(
+            printf "%s\n" "${categories[@]}" "${defaults[@]}" \
+            | tac \
+            | fzf \
+                --header="Current data directory: $dataPath" \
+                --preview "jq -r --arg category {} '.[] | select(.category == \$category) | .description' \"$dataPath/categories.json\" \"$dir/.default.json\";
+                          cat "$dir/.temp/{}" 2>/dev/null;
+                          if [[ {} == \"Install Selected\" ]]; then
+                              printf \"\n%s\n\" \"Currently Selected:\"
+                              cat "$dir/.temp/*" 2>/dev/null | grep '^\[\*\]' | sed 's/^\(\[\]\|\[\*\]\) //'
+                          fi" \
+        )
         #TODO: Add checkboxes selected and list of tasks to preview.
         case $choice in 
             "Install Selected")
